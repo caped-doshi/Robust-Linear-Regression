506ac4fdd41080f653aee193c3ed931b2bf094a7
myersjd <43558223+myersjd@users.noreply.github.com>
2023-04-16 14:18:10 -0400

    changed solver

--- RRM.py
+++ RRM.py
@@ -6,19 +6,66 @@ from sklearn.linear_model import HuberRegressor
 from matplotlib import pyplot as plt
 from sklearn.model_selection import train_test_split
 from scipy.optimize import minimize
+from scipy.special import rel_entr
+from scipy.stats import entropy
 
-def blockwise(X, y, epsilon):
-    
-    ##INITIALIZATION##
-    n, d = X.shape
-    p = np.ones([n,1])/n
-    
-    #initial theta as the solution of WLS at p_old
-    theta_old = np.linalg.pinv(X.T @ np.diag(p.flatten()) @ X) @ (X.T @ np.diag(p.flatten()) @ y)
 
+def data_loader_drug(i=0):
+    x = loadmat('C:/Users/Josh/Documents/data/qsar.mat')
+    x_train = x['X_train']
+    x_test = x['X_test']
+
+    y_train = x['y_train']
+    y_test = x['y_test']
+
+    x = np.concatenate((x_train, x_test), axis=0)
+    y = np.concatenate((y_train, y_test), axis=0).flatten()
+
+    intercept = np.ones((x.shape[0], 1))
+    x = np.concatenate((x, intercept), axis=1)
+
+    perm = np.random.permutation(len(y))
+
+    return x[perm], y[perm]
+
+
+def calc_RMSE(y, theta, X):
+  if len(y) != len(X): 
+    print("ERROR: MISMATCHING DATA/TARGET DIMENSIONS")
+    return None
+  if len(X) == 0: 
+    print("ERROR: NO DATA")
+    return None
+  if len(theta) != len(X[0]): 
+    print("ERROR: MISMATCHING DATA/COEF DIMENSIONS")
+    return None
+  return np.sqrt(np.mean((np.dot(X, theta) - y) ** 2))
+
+def LM(X, y):
+    return np.matmul(np.linalg.pinv(X),y)
+
+def addFeatureNoise(X,y,noise:float):
+  noisyY = np.copy(y)
+  noisyX = np.copy(X)
+  for i in range(int(len(y) * (1-noise)), len(y)):
+    noisyY[i] = 10000*y[i]
+    noisyX[i,:] = 100*X[i,:]
+  return noisyX, noisyY
+
+def addNoise(X, y, noise: float): 
+    noisyY = np.copy(y)
+    d = 100
+    #m_ = np.random.normal(4,4,d)
+    b_ = np.random.normal(5,5)
+    for i in range(int(len(y) * (1-noise)), len(y)):
+        noisyY[i] = b_
+        #noisyY[i] = np.random.normal(np.dot(np.transpose(m_),X[i,0:d]) + b_,0.01)
+    return noisyY
+
+
+
+def RRM(X, y, epsilon):
    
-    ##################
-
     ####RRM##########
     #INPUT
     #x_aug : n x d matrix
@@ -28,31 +75,43 @@ def blockwise(X, y, epsilon):
 
     #Output###
     #theta_new : d x 1 vector of robust estimate
+    
+    ##INITIALIZATION##
+    n, d = X.shape
+    p = np.ones([n,1])/n
+    
+    #initial theta as the solution of WLS at p_old
+    theta_old = np.linalg.pinv(X.T @ np.diag(p.flatten()) @ X) @ (X.T @ np.diag(p.flatten()) @ y)
+
 
 
     cnt = 1
 
     while (cnt==1):
+        #LOSS
         alpha = (y-X@theta_old)**2
 
-        p = p.flatten()
-        p = optimize(alpha.flatten(),epsilon,n, p).reshape(-1, 1)
+        p = optimize_RRM(alpha.flatten(),epsilon,n)
         # p = optimize_scipy(alpha.flatten(),epsilon,n, p).reshape(-1, 1)
             
+        #WLS SOLUTION
         theta_new  = np.linalg.pinv(X.T @ np.diag(p.flatten()) @ X) @ (X.T @ np.diag(p.flatten()) @ y)
 
-        if np.linalg.norm(theta_old-theta_new)/np.linalg.norm(theta_old)>1e-3:
-          #print(np.linalg.norm(theta_old-theta_new)/np.linalg.norm(theta_old))
+        if np.linalg.norm(theta_old-theta_new)/np.linalg.norm(theta_old)>1e-2:
+          print(np.linalg.norm(theta_old-theta_new)/np.linalg.norm(theta_old))
           cnt = 1
           theta_old = theta_new
         else:
             cnt = 0
     return theta_new
 
-def optimize(alpha, epsilon, n, p_val):
+
+
+def optimize_RRM(alpha, epsilon, n):
    # Define the optimization variables
   p = cp.Variable(n)
-  p.value = p_val
+  # p.value = p_val
+  p.value = np.ones(n)/n
 
   # Define the objective function to be minimized
   objective = cp.Minimize(alpha.T @ p)
@@ -64,40 +123,74 @@ def optimize(alpha, epsilon, n, p_val):
 
   # Define the problem and solve it
   prob = cp.Problem(objective, constraints)
-  prob.solve(solver=cp.ECOS, max_iters=500)# feastol=1e-3, abstol=1e-3, reltol=1e-3, max_iters=100)
+  prob.solve(solver=cp.SCS, eps=1e-3)# feastol=1e-3, abstol=1e-3, reltol=1e-3, max_iters=100)
   return p.value
 
-def optimize_scipy(alpha, epsilon, n, p_val):
-    def objective(p):
-        return alpha.T @ p
+# def optimize_scipy(alpha, epsilon, n, p_val):
+#     def objective(p):
+#         return alpha.T @ p
 
-    def constraint_entropy(p):
-        return entropy(p) - np.log((1 - epsilon) * n)
+#     def constraint_entropy(p):
+#         return entropy(p) - np.log((1 - epsilon) * n)
 
-    constraints = ({'type': 'eq', 'fun': lambda p: np.sum(p) - 1},  # probability constraint
-                   {'type': 'ineq', 'fun': constraint_entropy},  # entropy constraint
-                   {'type': 'ineq', 'fun': lambda p: -p})  # non-negativity constraint
+#     constraints = ({'type': 'eq', 'fun': lambda p: np.sum(p) - 1},  # probability constraint
+#                    {'type': 'ineq', 'fun': constraint_entropy},  # entropy constraint
+#                    {'type': 'ineq', 'fun': lambda p: -p})  # non-negativity constraint
 
-    res = minimize(objective, p_val, method='CG', constraints=constraints, options={'maxiter': 500})
+#     res = minimize(objective, p_val, method='CG', constraints=constraints, options={'maxiter': 500})
 
-    return res.x
+#     return res.x
 
-from data_loader import *
-from RMSE import *
-from noise_models.noise import *
+
 if __name__ == "__main__":
-    x_ = np.linspace(0.2,0.4,3)
-    X,y = data_loader_drug()
+    n = 2000
+    d = 100
+
+    meta_means_sever = []
+    meta_means_term = []
+    meta_means_subq = []
+    meta_means_crr = []
+    meta_means_genie = []
+    meta_means_ransac = []
+    meta_means_huber = []
+    meta_means_erm = []
+    meta_means_stir = []
+    meta_std_sever = []
+    meta_std_term = []
+    meta_std_subq = []
+    meta_std_crr = []
+    meta_std_ransac = []
+    meta_std_genie = []
+    meta_std_huber = []
+    meta_std_erm = []
+    meta_std_stir = []
+    x_ = np.linspace(0.1,0.4,11)
     for eps in x_:
-        print(f"epsilon:\t{eps}")
-        means = []
-        for j in range(4):
-            X_train, X_test, y_train, y_test = train_test_split(X, y, train_size = 0.8)
-            y_train_noisy = addUnstructuredNoise(X_train, y_train, eps)
-            theta = blockwise(X_train,y_train_noisy,eps)
-            loss = calc_RMSE(y_test, theta, X_test)
-            means.append(loss)
-            print(f"Loss:\t{loss:.3f}")
-        print(f"RRM:\t{np.mean(np.float32(means)):.3f}_{{({np.std(np.float32(means)):.4f})}}")
+        means_sever = []
+        means_term = []
+        means_subq = []
+        means_crr = []
+        means_genie = []
+        means_ransac = []
+        means_huber = []
+        means_erm = []
+        means_stir = []
+        for j in range(1):
+            X,y = data_loader_drug()
+            
+            X_train, X_test, y_train, y_test = train_test_split(X, y, train_size = .80)
+
+            y_train_noisy = addNoise(X_train, y_train, eps)
+
+
+            theta_rrm = RRM(X_train, y_train_noisy, eps)
+            loss_rrm = calc_RMSE(y_test,theta_rrm,X_test)
+            print("LOSS RRM", eps, loss_rrm)
+
+            theta_erm = LM(X_train, y_train_noisy)
+            loss_erm = calc_RMSE(y_test,theta_erm,X_test)
+            print("LOSS ERM", eps, loss_erm)
+
 
     
+
