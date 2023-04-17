2b8719b601577b37e7e5fbed366ee2bd655edcb8
myersjd <43558223+myersjd@users.noreply.github.com>
2023-04-17 10:54:05 -0400

    added argparser

--- sever.py
+++ sever.py
@@ -1,5 +1,8 @@
 import numpy as np
 from sklearn.linear_model import Ridge, LinearRegression
+import argparse
+from noise_models.noise import *
+from data_loader import *
 
 def SEVER(x_train, y_train, reg=2, p=0.01, iter=64):
     for _ in range(iter):
@@ -29,6 +32,9 @@ def SEVER(x_train, y_train, reg=2, p=0.01, iter=64):
         #in each iteration simply remove the top p fraction of outliers
         #according to the scores τi
         n_removed = int(p*len(tau))
+        if n_removed == len(x_train):
+            print("Warning: cannot remove p={} of values, too many iterations or p too large.".format(p))
+            continue
         # idx_kept = np.argpartition(tau, -n_removed)[:-n_removed]
         idx_kept = np.argsort(tau)[:-n_removed]
         idx_kept = np.sort(idx_kept)
@@ -36,7 +42,7 @@ def SEVER(x_train, y_train, reg=2, p=0.01, iter=64):
         y_train = y_train[idx_kept]
 
         # test_data = scaler.transform(x_test)
-        rmse = np.sqrt(np.mean((x_train@w - y_train)**2))
+        # rmse = np.sqrt(np.mean((x_train@w - y_train)**2))
         # print(rmse)
     return w
 
@@ -45,16 +51,59 @@ from data_loader import *
 from RMSE import *
 from noise_models.noise import *
 if __name__ == "__main__":
-    x_ = np.linspace(0.1,0.4,4)
-    X, y = gaussian(2000,200)
-    for eps in x_:
-        print(f"epsilon:\t{eps}")
-        means = []
-        for j in range(10):
-            X_train, X_test, y_train, y_test = train_test_split(X, y, train_size = 0.8)
-            y_train_noisy = addUnstructuredNoise(X_train, y_train, eps)
-            theta = SEVER(X_train,y_train_noisy)
-            loss = np.sqrt(np.mean((np.dot(X_test, theta) - y_test) ** 2))
-            means.append(loss)
-            print(f"Loss:\t{loss:.3f}")
-        print(f"Sever:\t{np.mean(np.float32(means)):.3f}_{{({np.std(np.float32(means)):.4f})}}")
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--num_trials',help='run how many times',type=int,default=5)
+    parser.add_argument('--iters',help='iterations of algorithm',type=int,default=64)
+    parser.add_argument('--reg',help='regularizer', type=float,default=2)
+    parser.add_argument('--p',help='fraction of outlier to remove (0:1)]', type=float,default=0.01)
+    parser.add_argument('--noise', help='noise ratio in range (0, 1)',type=float,default=0.1)
+    parser.add_argument('--noise_type',help="oblivious, adaptive, or feature",type=str,default='oblivious')
+    parser.add_argument('--dataset', help='dataset; drug, cal_housing, abalone, or synthetic',type=str,default='synthetic')
+    parser.add_argument('--n', help='samples for synthetic data',type=int,default='2000')
+    parser.add_argument('--d', help='dim for synthetic data',type=int,default='200')
+
+    parsed = vars(parser.parse_args())
+    num_trials = parsed['num_trials']
+    iters = parsed['iters']
+    reg = parsed['reg']
+    p = parsed['p']
+    noise = parsed['noise']
+    noise_type = parsed['noise_type']
+    dataset = parsed['dataset']
+
+    maxLen = max([len(ii) for ii in parsed.keys()])
+    fmtString = '\t%' + str(maxLen) + 's : %s'
+    print('Arguments:')
+    for keyPair in sorted(parsed.items()): print(fmtString % keyPair)
+
+    if dataset == 'cal_housing':
+        X, y = data_loader_cal_housing()
+    elif dataset == 'abalone':
+        X, y = data_loader_abalone()
+    elif dataset == 'drug':
+        X, y = data_loader_drug()
+    elif dataset == 'synthetic':
+        n = parsed['n']
+        d = parsed['d']
+        X, y = gaussian(n, d)
+
+    if noise_type == "oblivious":
+        noise_fn = addObliviousNoise
+    if noise_type == "adaptive":
+        noise_fn = addAdaptiveNoise
+    if noise_type == "feature":
+        noise_fn = addFeatureNoise
+
+    print(f"Epsilon:\t{noise}")
+    means = []
+    for _ in range(num_trials):
+        X_train, X_test, y_train, y_test = train_test_split(X, y, train_size = 0.8)
+
+        X_train, y_train_noisy = noise_fn(X, y, noise)
+
+        theta = SEVER(X_train,y_train_noisy, reg=reg, p=p, iter=iters)
+        loss = np.sqrt(np.mean((np.dot(X_test, theta) - y_test) ** 2))
+        means.append(loss)
+        print(f"Loss:\t{loss:.3f}")
+    print(f"SEVER:\t{np.mean(np.float32(means)):.3f}_{{({np.std(np.float32(means)):.4f})}}")
