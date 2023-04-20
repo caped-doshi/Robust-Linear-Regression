1b291cc84a779aff4db19713adcbcd5ca99e5c78
Fatih Orhan <orhanfatih2002@gmail.com>
2023-04-20 16:26:27 -0400

    added argparser to smart

--- robust-linear-regression/smart.py
+++ robust-linear-regression/smart.py
@@ -48,17 +48,54 @@ if __name__ == "__main__":
     
     parser = argparse.ArgumentParser()
     
-    X,y = data_loader_drug()
+    parser.add_argument('--dataset', help='dataset; drug, cal_housing, abalone, or synthetic',type=str,default='drug')
+    parser.add_argument('--num_trials',help='run how many times',type=int,default=5)
+    parser.add_argument('--R_val',help='how many iterations of algorithm',type=float,default=10)
+    parser.add_argument('--eta_val',help='how many iterations of algorithm',type=float,default=0.001)
+    parser.add_argument('--noise_type',help="oblivious, adaptive, or feature",type=str,default='oblivious')
+    parser.add_argument('--noise', help='noise ratio in range (0, 1)',type=float,default=0.1)
+    parser.add_argument('--n', help='samples for synthetic data',type=int,default='2000')
+    parser.add_argument('--d', help='dim for synthetic data',type=int,default='200')
+    
+    parsed = vars(parser.parse_args())
+    
+    dataset = parsed['dataset']
+    num_trials = parsed['num_trials']
+    eta_value = parsed['eta_val']
+    R_value = parsed['R_val']
+    noise_type = parsed['noise_type']
+    noise = parsed['noise']
+    
+    if dataset == 'cal_housing':
+        X, y = data_loader_cal_housing()
+    elif dataset == 'abalone':
+        X, y = data_loader_abalone()
+    elif dataset == 'drug':
+        X, y = data_loader_drug()     
+    elif dataset == 'synthetic':
+        n = parsed['n']
+        d = parsed['d']
+        X, y = gaussian(n, d) 
+        
+    if noise_type == "oblivious":
+        noise_fn = addObliviousNoise
+    if noise_type == "adaptive":
+        noise_fn = addAdaptiveNoise
+    if noise_type == "feature":
+        noise_fn = addFeatureNoise
+        
+    maxLen = max([len(ii) for ii in parsed.keys()])
+    fmtString = '\t%' + str(maxLen) + 's : %s'
+    print('Arguments:')
+    for keyPair in sorted(parsed.items()): print(fmtString % keyPair)
 
-    x_ = np.linspace(0.1,0.4,4)
-    for eps in x_:
-        print(f"epsilon:\t{eps}")
-        means = []
-        for j in range(10):
-            X_train, X_test, y_train, y_test = train_test_split(X, y, train_size = 0.8)
-            y_train_noisy = addUnstructuredNoise(X_train, y_train, eps)
-            theta = TMLE(X_train,y_train_noisy, eps,  0.001, 1e9,f)
-            loss = calc_RMSE(y_test, theta, X_test)
-            means.append(loss)
-            print(f"Loss:\t{loss:.3f}")
-        print(f"SMART:\t{np.mean(np.float32(means)):.3f}_{{({np.std(np.float32(means)):.4f})}}")
+    print(f"epsilon:\t{noise}")
+    means = []
+    for j in range(num_trials):
+        X_train, X_test, y_train, y_test = train_test_split(X, y, train_size = 0.8)
+        y_train_noisy = noise_fn(X_train, y_train, noise)
+        theta = TMLE(X_train,y_train_noisy, noise, eta_value, R_value,f)
+        loss = np.sqrt(np.mean((np.dot(X_test, theta) - y_test) ** 2))
+        means.append(loss)
+        print(f"Loss:\t{loss:.3f}")
+    print(f"SMART:\t{np.mean(np.float32(means)):.3f}_{{({np.std(np.float32(means)):.4f})}}")
--- robust-linear-regression/term.py
+++ robust-linear-regression/term.py
@@ -44,6 +44,7 @@ if __name__ == "__main__":
     parser.add_argument('--dataset', help='dataset; drug, cal_housing, abalone, or synthetic',type=str,default='drug')
     parser.add_argument('--num_trials',help='run how many times',type=int,default=5)
     parser.add_argument('--num_iters',help='how many iterations of algorithm',type=int,default=64)
+    parser.add_argument('--noise_type',help="oblivious, adaptive, or feature",type=str,default='oblivious')
     parser.add_argument('--noise', help='noise ratio in range (0, 1)',type=float,default=0.1)
     parser.add_argument('--n', help='samples for synthetic data',type=int,default='2000')
     parser.add_argument('--d', help='dim for synthetic data',type=int,default='200')
@@ -53,6 +54,7 @@ if __name__ == "__main__":
     dataset = parsed['dataset']
     num_trials = parsed['num_trials']
     num_iters = parsed['num_iters']
+    noise_type = parsed['noise_type']
     noise = parsed['noise']
     
     if dataset == 'cal_housing':
@@ -65,6 +67,13 @@ if __name__ == "__main__":
         n = parsed['n']
         d = parsed['d']
         X, y = gaussian(n, d) 
+        
+    if noise_type == "oblivious":
+        noise_fn = addObliviousNoise
+    if noise_type == "adaptive":
+        noise_fn = addAdaptiveNoise
+    if noise_type == "feature":
+        noise_fn = addFeatureNoise
     
     # X,y = gaussian(100000,500)
     
@@ -78,7 +87,7 @@ if __name__ == "__main__":
     means = []
     for j in range(num_trials):
         X_train, X_test, y_train, y_test = train_test_split(X, y, train_size = 0.8)
-        X_train, y_train_noisy = addObliviousNoise(X_train, y_train, noise)
+        X_train, y_train_noisy = noise_fn(X_train, y_train, noise)
         theta = TERM(X_train,y_train_noisy, -2, 0.01, num_iters)
         loss = np.sqrt(np.mean((np.dot(X_test, theta) - y_test) ** 2))
         means.append(loss)
